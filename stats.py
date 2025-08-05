3eed2cecb28bd4ebc33c4f607fe62dc2dd92203f
HARSHITH PADI <115480440+harshithpadi@users.noreply.github.com>
2025-08-05 16:12:53 +0530

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,4 +1,8 @@
 
 def calculateStats(numbers):
   # implement the computation of statistics here and return the results
+  stats = {}
+  stats['avg'] = sum(numbers)/len(numbers)
+  stats['min'] = min(numbers)
+  stats['max'] = max(numbers)                                
   return None
